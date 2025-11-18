9ede7546da7e696dff33dfda326e8c48bbcb8993
vinod-ayckattu <vinodharidas.dev24@gmail.com>
2025-11-18 18:40:16 +0530

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use APp\Models\User;
+
+class UserController extends Controller
+{
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|min:6|max:15',
+            'gender' => 'required'
+        ]);
+
+       // $photo = $request->file('photo')->store('/photos');
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        //    'photo' => $photo,
+            'gender' => $request->gender,
+        ]);
+
+        return response()->json([
+            'status' => 200,
+            'user' => $user 
+        ]);
+
+
+
+    }
+}
---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Post;
+
+class DatabaseSeeder extends Seeder
+{
+    use WithoutModelEvents;
+
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+       // User::factory(25)->create();
+        Post::factory(50)->create();
+
+      /*  User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);*/
+    }
+}
